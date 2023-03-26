e71c14630cbebe6949f8d38ebc3caac33ded225e
DevOp972 <100531674+devSmarak972@users.noreply.github.com>
2023-03-26 09:25:02 +0530

    Initial commit

---
+++ UDIS/mainapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+# import Http Response from django
+from django.shortcuts import render
+
+# create a function
+def dashboard(request):
+	# create a dictionary to pass
+	# data to the template
+	context ={
+		"data":"Gfg is the best",
+		"list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	}
+	# return response with template and context
+	return render(request, "dashboard.html", context)
