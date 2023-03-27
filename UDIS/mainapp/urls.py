4144eaf92707c498a8e1ef96e530d40b950fa638
DevOp972 <100531674+devSmarak972@users.noreply.github.com>
2023-03-27 14:20:17 +0530

    changes all

--- UDIS/mainapp/urls.py
+++ UDIS/mainapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 #now import the views.py file into this code
 from . import views
 urlpatterns=[
-  path('',views.dashboard)
+  path('',views.dashboard),
+  path('dashboard',views.dashboard),
+  path('professors',views.research),
+  path('calendar',views.calendar),
+  path('student-profile',views.profile),
 ]
--- UDIS/mainapp/views.py
+++ UDIS/mainapp/views.py
@@ -9,8 +9,65 @@ def dashboard(request):
 	# create a dictionary to pass
 	# data to the template
 	context ={
-		"data":"Gfg is the best",
+		"data":"some data",
 		"list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 	}
 	# return response with template and context
 	return render(request, "dashboard.html", context)
+def research(request):
+	# create a dictionary to pass
+	# data to the template
+	context ={
+		"data":"some data",
+		"list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	}
+	# return response with template and context
+	return render(request, "ProjectsPublications/research.html", context)
+def calendar(request):
+	# create a dictionary to pass
+	# data to the template
+	context ={
+		"user":"Secretary",
+		"list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	}
+	# return response with template and context
+	return render(request, "calendar.html", context)
+def profile(request):
+	# create a dictionary to pass
+	# data to the template
+	courses=[{"subno":"CS21204","name":"Formal Language Automata Theory","url":"/coursepage","type":"DEPTH CORE","LTP":"3-1-0","credits":"4.0","faculty":"Animesh Mukherjee , Soumyajit Dey","status":"Registered"},{"subno":"CS21204","name":"Formal Language Automata Theory","url":"/coursepage","type":"DEPTH CORE","LTP":"3-1-0","credits":"4.0","faculty":"Animesh Mukherjee , Soumyajit Dey","status":"Registered"},{"subno":"CS21204","name":"Formal Language Automata Theory","url":"/coursepage","type":"DEPTH CORE","LTP":"3-1-0","credits":"4.0","faculty":"Animesh Mukherjee , Soumyajit Dey","status":"Registered"},{"subno":"CS21204","name":"Formal Language Automata Theory","url":"/coursepage","type":"DEPTH CORE","LTP":"3-1-0","credits":"4.0","faculty":"Animesh Mukherjee , Soumyajit Dey","status":"Registered"},{"subno":"CS21204","name":"Formal Language Automata Theory","url":"/coursepage","type":"DEPTH CORE","LTP":"3-1-0","credits":"4.0","faculty":"Animesh Mukherjee , Soumyajit Dey","status":"Registered"},{"subno":"CS21204","name":"Formal Language Automata Theory","url":"/coursepage","type":"DEPTH CORE","LTP":"3-1-0","credits":"4.0","faculty":"Animesh Mukherjee , Soumyajit Dey","status":"Registered"}]    
+	sgpa=[9.76,9.84,9.44,9.28]
+	context ={
+		"user":"Student",
+		"backlogs":0,
+		"courses":courses,
+		"cgpa":9.29,
+		"name":"Smarak Kanjilal",
+		"rollno":"21CS30061",
+		"gender":"Male",
+		"department":"Department of Computer Science and Engineering",
+		"course":"M.Tech Dual Degree 5Y",
+		"nationality":"Indian",
+		"hall":"Azad Hall",
+		"aadhar":"531220019",
+		"status":"On Roll",
+		"EAA":"NSS - Unit 7",
+		"adm_year": 2021,
+		"adm_nature":"JEE",
+		"primary_email":"smarakkanjilal@gmail.com",
+		"inst_email":"smarakkanjilal@kgpian.iitkgp.ac.in",
+		"teams_pass":"1jbs672h",
+		"mobile":"9330223389",
+		"guardian_email":"guardian@g.co",
+		"address":"Harmony apartment, 28/1B, Nakuleswar Bhattacharya Lane ,Kolkata-70035",
+		"location":"Kolkata",
+		"pin":"700035",
+		"state":"West Bengal",
+		"cleared_sem_crd":24.0,
+		"sem_crd":24.0,
+		"cleared_tot_crd":73.0,
+		"tot_crd":73.0,
+		"sgpa":sgpa[-1]
+	}
+	# return response with template and context
+	return render(request, "student-profile.html", context)
