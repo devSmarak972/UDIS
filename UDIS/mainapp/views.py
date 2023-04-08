ae972c034baf87f3bf854ba5bc2adedc86d267e4
DevOp972 <100531674+devSmarak972@users.noreply.github.com>
2023-04-08 19:48:00 +0530

    user

--- UDIS/mainapp/views.py
+++ UDIS/mainapp/views.py
@@ -213,7 +213,7 @@ def profile(request, rollno):
 
     print(today.year, rollno)
     student = Student.objects.get(rollno=rollno)
-    courses = student.courses.all()
+    courses = student.regcourses.all()
 
     print(student)
     context = {
